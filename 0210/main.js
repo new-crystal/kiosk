337688be90425b2ae4b8b213b2ca58e82034c206
new-crystal <oj3339@naver.com>
2023-01-27 14:22:20 +0900

    signup btn event

--- 0210/main.js
+++ 0210/main.js
@@ -1,18 +1,26 @@
 "use strict";
 
-const wrap = document.querySelectorAll(".wrap") 
+const container = document.querySelector(".container") 
 const homePage = document.querySelector(".homepage")
 const secondPage = document.querySelector(".second-page")
 const thirdPage = document.querySelector(".third-page")
 const fourthPage = document.querySelector(".fourth-page")
 const btnBox = document.querySelector(".btn-box")
+const texts = document.querySelectorAll(".text")
+const img1 = document.querySelector(".third-1")
+const img2 = document.querySelector(".third-2")
+const img3 = document.querySelector(".fourth-1")
+const img4 = document.querySelector(".fourth-2")
+const img5 = document.querySelector(".fourth-3")
+let signUp = false;
 
 btnBox.addEventListener("click", (e)=>{onClickBtn(e)})
 
 function onClickBtn(event){
-  
  if(event.target.className === "home"){
     gofirstPage(true)
+ }else if(event.target.className === "sign-up"){
+    makeSignUp()
  }
 }
 
@@ -20,25 +28,25 @@ function init(){
     goSecondPage();
 }
 
+
 function goSecondPage(){
-   console.log(btnBox)
-    const texts = document.querySelectorAll(".text")
     setTimeout(()=>{
         homePage.classList.remove("page-visible")
         homePage.classList.add("page-hidden")
         secondPage.classList.remove("page-hidden")
         secondPage.classList.add("page-visible")
+        secondPage.classList[1] === "page-visible" && goThirdPage();
         texts.forEach((text)=>{
             text.style.animation = "fadeInleft 1s"
         })
-        secondPage.classList[1] === "page-visible" && goThirdPage();
     },7000)
 }  
 
 function goThirdPage(){
-    const img1 = document.querySelector(".third-1")
-    const img2 = document.querySelector(".third-2")
     setTimeout(()=>{
+        texts.forEach((text)=>{
+            text.style.animation = ""
+        })
         secondPage.classList.remove("page-visible")
         secondPage.classList.add("page-hidden")
         thirdPage.classList.remove("page-hidden")
@@ -50,11 +58,9 @@ function goThirdPage(){
 }
 
 function gofourthPage(){
-    const img3 = document.querySelector(".fourth-1")
-    const img4 = document.querySelector(".fourth-2")
-    const img5 = document.querySelector(".fourth-3")
-
     setTimeout(()=>{
+        img1.style.animation = ""
+        img2.style.animation =""
         thirdPage.classList.remove("page-visible")
         thirdPage.classList.add("page-hidden")
         fourthPage.classList.remove("page-hidden")
@@ -67,8 +73,10 @@ function gofourthPage(){
 }
 
 function gofirstPage(time){
-    console.log(time)
     if(time === true){
+        img3.style.animation = ""
+        img4.style.animation = ""
+        img5.style.animation = ""
         secondPage.classList.remove("page-visible")
         secondPage.classList.add("page-hidden")
         thirdPage.classList.remove("page-visible")
@@ -80,6 +88,9 @@ function gofirstPage(time){
          homePage.classList[1] === "page-visible" && goSecondPage();
     }else if (time === false){
         setTimeout(()=>{
+            img3.style.animation = ""
+            img4.style.animation = ""
+            img5.style.animation = ""
             secondPage.classList.remove("page-visible")
             secondPage.classList.add("page-hidden")
             thirdPage.classList.remove("page-visible")
@@ -92,4 +103,20 @@ function gofirstPage(time){
             },7000)
     }
 }
-init()
+
+function makeSignUp(){
+    signUp = !signUp
+    const iframe = document.createElement("iframe")
+    if(signUp === true){ 
+        iframe.setAttribute("class", "iframe")
+        iframe.setAttribute("src", "https://www.organon.com/korea/")
+        iframe.setAttribute("width", "800")
+        iframe.setAttribute("height", "700")
+        iframe.setAttribute("scrolling","yes")
+       container.appendChild(iframe)
+    }else if(signUp === false && container.childNodes[11] !== undefined){
+        container.childNodes[11].remove()
+    }
+}
+
+init();
