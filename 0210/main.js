facbdcd0f25bf0504d29aed61483c5f53ba5bacd
new-crystal <oj3339@naver.com>
2023-01-27 12:13:33 +0900

    click event 테스트중

--- 0210/main.js
+++ 0210/main.js
@@ -1,19 +1,38 @@
 "use strict";
 
-const wrap = document.querySelector(".wrap");
-const wrapRect = wrap.getBoundingClientRect();
-const home = document.querySelector("#home")
-const signUp = document.querySelector("#sign-up")
 const homePage = document.querySelector(".homepage")
 const secondPage = document.querySelector(".second-page")
 const thirdPage = document.querySelector(".third-page")
 const fourthPage = document.querySelector(".fourth-page")
+const btnBoxes = document.querySelectorAll(".btn-box")
+const firstBtnBox = document.querySelector(".first-btn-box")
+
+
+// const firstHomeBtn = document.querySelector("#home-1")
+// const firstSignUpBtn = document.querySelector("#sign-up-1")
+// const secondHomeBtn = document.querySelector("#home-2")
+// const SecondSignUpBtn = document.querySelector("#sign-up-2")
+// const ThridHomeBtn = document.querySelector("#home-3")
+// const ThridSignUpBtn = document.querySelector("#sign-up-3")
+// const fourthHomeBtn = document.querySelector("#home-4")
+// const fourthSignUpBtn = document.querySelector("#sign-up-4")
+
+// firstHomeBtn.addEventListener("click",()=>{ gofirstPage(true)})
+// secondHomeBtn.addEventListener("click",()=>{ console.log("click")})
+// ThridHomeBtn.addEventListener("click",()=>{ gofirstPage(true)})
+// fourthHomeBtn.addEventListener("click",()=>{ gofirstPage(true)})
+
+firstBtnBox.addEventListener("click", (e)=>{console.log("클릭", e)})
+btnBoxes.forEach((btnBox)=>{
+    btnBox.addEventListener("click", (e)=>{console.log("click",e)})
+})
 
 function init(){
     goSecondPage();
 }
 
 function goSecondPage(){
+   
     const texts = document.querySelectorAll(".text")
     setTimeout(()=>{
         homePage.classList.remove("page-visible")
@@ -54,23 +73,33 @@ function gofourthPage(){
         img3.style.animation = "fadeInUp 1s"
         img4.style.animation = "fadeInUp 2s"
         img5.style.animation = "fadeInUp 3s"
-        fourthPage.classList[1] === "page-visible" && gofirstPage();
+        fourthPage.classList[1] === "page-visible" && gofirstPage(false);
     },7000)
 }
 
-function gofirstPage(){
-    setTimeout(()=>{
-    secondPage.classList.remove("page-visible")
-    secondPage.classList.add("page-hidden")
-    thirdPage.classList.remove("page-visible")
-    thirdPage.classList.add("page-hidden")
-    fourthPage.classList.remove("page-visible")
-    fourthPage.classList.add("page-hidden")
-    homePage.classList.remove("page-hidden")
-    homePage.classList.add("page-visible")
-     homePage.classList[1] === "page-visible" && goSecondPage();
-    },7000)
+function gofirstPage(time){
+    if(time){
+        secondPage.classList.remove("page-visible")
+        secondPage.classList.add("page-hidden")
+        thirdPage.classList.remove("page-visible")
+        thirdPage.classList.add("page-hidden")
+        fourthPage.classList.remove("page-visible")
+        fourthPage.classList.add("page-hidden")
+        homePage.classList.remove("page-hidden")
+        homePage.classList.add("page-visible")
+         homePage.classList[1] === "page-visible" && goSecondPage();
+    }else if (!time){
+        setTimeout(()=>{
+            secondPage.classList.remove("page-visible")
+            secondPage.classList.add("page-hidden")
+            thirdPage.classList.remove("page-visible")
+            thirdPage.classList.add("page-hidden")
+            fourthPage.classList.remove("page-visible")
+            fourthPage.classList.add("page-hidden")
+            homePage.classList.remove("page-hidden")
+            homePage.classList.add("page-visible")
+             homePage.classList[1] === "page-visible" && goSecondPage();
+            },7000)
+    }
 }
 init()
-
-home.addEventListener("touchstart", () => {gofirstPage()})
