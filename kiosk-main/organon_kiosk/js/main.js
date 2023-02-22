7aa4088e971a8be3f792ffb6e083d619d7987250
new-crystal <oj3339@naver.com>
2023-02-22 12:17:05 +0900

    module remove

--- kiosk-main/organon_kiosk/js/main.js
+++ kiosk-main/organon_kiosk/js/main.js
@@ -1,24 +1,179 @@
 "use strict";
+class Page {
+  order = 1;
 
-//import { Page } from "./page.js";
-//import { Iframe } from "./iframe.js";
+  //페이지
+  homePage = document.querySelector(".homepage");
+  secondPage = document.querySelector(".second-page");
+  thirdPage = document.querySelector(".third-page");
+  fourthPage = document.querySelector(".fourth-page");
+  iframePage = document.querySelector(".iframe-page");
 
-let Page = null;
-let Iframe = null;
-let page = null;
-let iframe = null;
+  //애니메이션 요소
+  text1 = document.querySelector(".first-animation-down");
+  btnBox = document.querySelector(".btn-box");
+  text2 = document.querySelector(".text-1");
+  text3 = document.querySelector(".text-2");
+  img1 = document.querySelector(".third-1");
+  img2 = document.querySelector(".third-2");
+  img3 = document.querySelector(".fourth-1");
+  img4 = document.querySelector(".fourth-2");
+  img5 = document.querySelector(".fourth-3");
+  fourthImg = document.querySelector(".fourth-img-box");
+  bubbles = document.querySelectorAll(".bubble");
 
-async function load() {
-  Page = await import("./page.js");
-  Iframe = await import("./iframe.js");
+  //첫번째 페이지 작동 함수
+  gofirst() {
+    //다른 페이지 숨기기
+    this.secondPage.classList.add("page-hidden");
+    this.thirdPage.classList.add("page-hidden");
+    this.fourthPage.classList.add("page-hidden");
+
+    // 애니메이션 초기화
+    this.text2.style.animation = "";
+    this.text3.style.animation = "";
+    this.img1.style.animation = "";
+    this.img2.style.animation = "";
+    this.img3.style.animation = "";
+    this.img4.style.animation = "";
+    this.img5.style.animation = "";
+
+    //페이지 보이기
+    this.homePage.classList.remove("page-hidden");
+
+    //페이지 애니메이션
+    this.bubbles.forEach((bubble) => {
+      bubble.style.opacity = 1;
+    });
+    this.text1.style.animation = `fadeInDown 1s`;
+  }
+
+  //두번째 페이지 작동 함수
+  goSecond() {
+    //다른 페이지 숨기기
+    this.homePage.classList.add("page-hidden");
+    this.thirdPage.classList.add("page-hidden");
+    this.fourthPage.classList.add("page-hidden");
+
+    //전 페이지 애니메이션 초기화
+    this.text1.style.animation = "";
+    this.bubbles.forEach((bubble) => {
+      bubble.style.opacity = 0;
+    });
+
+    //페이지 보이기
+    this.secondPage.classList.remove("page-hidden");
+
+    //페이지 애니메이션
+    this.text2.style.animation = `fadeInleft 1s`;
+    this.text3.style.animation = `fadeInleft 1.5s`;
+  }
+
+  //세번째 페이지 작동 함수
+  goThird() {
+    //다른 페이지 숨기기
+    this.homePage.classList.add("page-hidden");
+    this.secondPage.classList.add("page-hidden");
+    this.fourthPage.classList.add("page-hidden");
+
+    //전 페이지 애니메이션 초기화
+    this.text2.style.animation = "";
+    this.text3.style.animation = "";
+
+    //페이지 보이기
+    this.thirdPage.classList.remove("page-hidden");
+
+    //페이지 애니메이션
+    this.img1.style.animation = `fadeInRight 1s`;
+    this.img2.style.animation = `fadeInRight 2s`;
+  }
+
+  //네번째 페이지 작동 함수
+  gofourth() {
+    //다른 페이지 숨기기
+    this.homePage.classList.add("page-hidden");
+    this.secondPage.classList.add("page-hidden");
+    this.thirdPage.classList.add("page-hidden");
+
+    //전 페이지 애니메이션 초기화
+    this.img1.style.animation = "";
+    this.img2.style.animation = "";
+
+    //페이지 보이기
+    this.fourthPage.classList.remove("page-hidden");
+
+    //페이지 애니메이션
+    this.img3.style.animation = `fadeInUp 1s`;
+    this.img4.style.animation = `fadeInUp 2s`;
+    this.img5.style.animation = `fadeInUp 3s`;
+  }
+
+  //Iframe 보여주는 함수
+  showIframe() {
+    //비눗방울
+    this.bubbles.forEach((bubble) => {
+      bubble.style.opacity = 1;
+    });
+    //모든 애니메이션 초기화
+    this.text1.style.animation = "";
+    this.text2.style.animation = "";
+    this.text3.style.animation = "";
+    this.img1.style.animation = "";
+    this.img2.style.animation = "";
+    this.img3.style.animation = "";
+    this.img4.style.animation = "";
+    this.img5.style.animation = "";
+
+    //모든 페이지 숨기기
+    this.homePage.classList.add("page-hidden");
+    this.secondPage.classList.add("page-hidden");
+    this.thirdPage.classList.add("page-hidden");
+    this.fourthPage.classList.add("page-hidden");
+  }
 }
 
-load().then(() => {
-  page = new Page.Page();
-  iframe = new Iframe.Iframe();
-  startInterval();
-  interval();
-});
+class Iframe {
+  //회원가입 상태 = false
+  //true -> 회원 가입 버튼 누를 경우 -> iframe, closed button 생성
+  signUp = false;
+
+  //페이지 전체
+  container = document.querySelector(".container");
+
+  //iframe
+  iframeBox = document.querySelector(".iframe-page");
+  btnBox = document.querySelector(".btn-box");
+  iframeContent = null;
+
+  signUpBtn = document.querySelector(".sign-up");
+  closeBtn = document.querySelector(".close-btn");
+
+  close() {
+    this.signUp = false;
+    this.iframeContent = null;
+    this.iframeBox.classList.add("iframe-page-hidden");
+    this.container.style.backgroundColor = "rgba(0,0,0,0)";
+    this.btnBox.style.filter = "brightness(100%)";
+  }
+
+  create() {
+    this.signUp = true;
+    this.iframeBox.classList.remove("iframe-page-hidden");
+    this.iframeContent = document.createElement("iframe");
+    this.iframeContent.classList.add("iframe");
+    this.iframeContent.src =
+      "https://organonpro.com/kr-kr/member-option/?screenToRender=traditionalRegistration";
+    this.iframeContent.width = 900;
+    this.iframeContent.height = 1200;
+    this.iframeBox.appendChild(this.iframeContent);
+
+    this.container.style.backgroundColor = "rgba(0,0,0,0.3)";
+    this.btnBox.style.filter = "brightness(80%)";
+  }
+}
+
+const page = new Page();
+const iframe = new Iframe();
 
 //clearInterval을 하기 위한 setInterval Id
 let intervalId = null;
@@ -28,16 +183,40 @@ const home = document.querySelector(".home");
 const signUpBtn = document.querySelector(".sign-up");
 const closeBtn = document.querySelector(".close-btn");
 
-//배경음악 재생 함수
-async function playBackGroundAudio() {
-  const audio = new Audio("sound/bg.mp3");
-  audio.loop = true;
-  audio.play();
+//window load 이후 배경음악 권한 허용 함수
+let AudioContext;
+let audioContext;
+
+//배경음악 디버깅 함수
+window.onload = function () {
+  const bgSound = new Audio("../sound/bg.mp3");
+  console.log(bgSound);
+  navigator.mediaDevices
+    .getUserMedia({ audio: true })
+    .then(() => {
+      AudioContext = window.AudioContext;
+      audioContext = new AudioContext();
+      playSound(bgSound);
+    })
+    .catch((e) => {
+      console.error(`Audio permissions denied: ${e}`);
+    });
+};
+
+//배경음악 재생함수
+function playSound(sound) {
+  const bgsong = sound.play();
+  sound.loop = true;
+  if (bgsong !== undefined) {
+    bgsong;
+  }
 }
 
 //윈도우가 로드됐을 때 배경음악 재생함수 실행
 window.onload = () => {
-  playBackGroundAudio();
+  //playBackGroundAudio();
+  startInterval();
+  interval();
 };
 
 //마우스 우클릭 방지 이벤트
